5e3caf5add685457844756e33998422cdf9757fb
denver6000 <denverballesteros01@gmail.com>
2024-06-22 04:03:11 +0800

    added an after_install

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-ls
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 17
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm use 17
-npm i 
-npm i pm2 - g
-npm i typescript
-npm run build
