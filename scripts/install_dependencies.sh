d072c75c8746ecb36d4bd7a1d69b72e003a20f87
denver6000 <denverballesteros01@gmail.com>
2024-06-22 03:49:49 +0800

    debug

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+ls
 nvm use 17
 npm i 
 npm i pm2 - g
