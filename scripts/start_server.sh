322ed0e0b22cc3114d04a5f3046803e4dbe3d5d3
denver6000 <denverballesteros01@gmail.com>
2024-06-22 05:58:45 +0800

    fixed npm not found, hopefully

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 cd var/node/app
 ls
 which pm2
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/bin/bash\
+
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 cd var/node/app
 set -e
