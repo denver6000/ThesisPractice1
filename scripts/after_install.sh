6c6c3002ee2a54f00f13c6c1251a769226e5b09b
denver6000 <denverballesteros01@gmail.com>
2024-06-22 04:41:06 +0800

    edited after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-ls
 cd var/node/app
-echo "cd"
-ls
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm use 17
 npm i
 npm i @types/node
